e5e5dcdfd14d60a5170c6ee98febf0f9bce6d7e0
jimmorrisson <pyszczakjakub@gmail.com>
2021-03-25 00:00:33 +0100

    Compile script updated

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd src
-javac -cp ".:/Users/admin/git/ChessCommon/ChessCommon.jar:/Users/admin/java_proj/json-20201115.jar" *.java
-java -cp ".:/Users/admin/git/ChessCommon/ChessCommon.jar:/Users/admin/java_proj/json-20201115.jar" App
+javac -cp ".:../ChessCommon.jar:../json-20201115.jar" *.java
+java -cp ".:../ChessCommon.jar:../json-20201115.jar" App
